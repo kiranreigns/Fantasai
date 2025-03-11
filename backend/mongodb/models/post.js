43218fc9a397e2019b229af3e68a01938020c9f8
Kiran <kiranreigns@hotmail.com>
2025-03-11 18:32:31 +0530

    Update User and Post schemas

--- backend/mongodb/models/post.js
+++ backend/mongodb/models/post.js
@@ -1,10 +1,11 @@
 import mongoose from "mongoose";
-const Post = new mongoose.Schema({
+const PostSchema = new mongoose.Schema({
   name: { type: String, required: true },
   prompt: { type: String, required: true },
   photo: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
 });
 
-const PostSchema = mongoose.model("Post", Post);
+const Post = mongoose.model("Post", PostSchema);
 
-export default PostSchema;
+export default Post;
--- backend/mongodb/models/user.js
+++ backend/mongodb/models/user.js
@@ -1,14 +1,12 @@
-// models/user.js
 import mongoose from "mongoose";
 
-const User = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String },
-  googleId: { type: String },
   createdAt: { type: Date, default: Date.now },
 });
 
-const UserSchema = mongoose.model("User", User);
+const User = mongoose.model("User", UserSchema);
 
-export default UserSchema;
+export default User;
